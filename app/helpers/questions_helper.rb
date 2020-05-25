cc3440bbc5a47fe8526fb125e05b5db6628fe7d6
ajlehenbauer <46498457+ajlehenbauer@users.noreply.github.com>
2020-05-24 21:13:06 -0400

    another minor fix

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -2,7 +2,7 @@ module QuestionsHelper
     
     def increment_rep(username)
         @q = User.find_by(name: username)
-        if !@q.nil?
+        if !@q.nil? && username != current_user.name
             if @q.reputation.nil?
                 @q.update_attribute(:reputation,10)
             else
